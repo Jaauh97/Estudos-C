42aea97c55e601f32185cb16ab85a0d802179044
Joao Pedro <jaauhp@gmail.com>
2023-10-18 15:42:20 -0300

    up 02

---
+++ Ex02 Saida de dados/main.c
@@ -0,0 +1,17 @@
+int main(int argc, char const *argv[])
+{
+    printf("Ola , Meu Nome e joao. \n");
+
+    
+    printf("Eu tambem tenho: %d.\n", 27);
+   
+    printf("Valor Real: %f.\n", 3.14159265);
+   
+    printf("Valor Real com apenas duas casas:  %.2f.\n", 3.14159265);
+    
+    printf("Dados de texto: %c.\n", 'A');
+
+    printf("Dados de texto: %s.\n", "Testando");
+
+    return 0;
+}
