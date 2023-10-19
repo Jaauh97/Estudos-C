e192e98e0f9abef1f349f86b9e6459d7efd44481
Joao Pedro <jaauhp@gmail.com>
2023-10-19 18:23:12 -0300

    aula 06

--- ex07/main.c
+++ ex07/main.c
@@ -1,5 +1,14 @@
 int main(int argc, char const *argv[])
 {
+    float n;
+
+    printf("Escreva Sua Nota: \n");
+    scanf("%f", &n);
+
+    if (n >= 4.0 && n < 7.0);
+    {
+        printf("Voce Esta de Recuperacao !! \n");
+    }
     
     return 0;
 }
