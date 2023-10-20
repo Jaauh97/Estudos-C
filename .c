fc901c03d26f51c8e2749cf75c0cda082ec39a2c
Joao Pedro <jaauhp@gmail.com>
2023-10-20 12:30:42 -0300

    update aula 8

---
+++ .c
@@ -0,0 +1,18 @@
+int main(int argc, char *argv[]) {
+	
+	float m;
+	
+	printf("Insira a nota: \n");
+	scanf("%f",&m);
+	
+	if (m >= 7.0){
+		printf("Aprovado \n");
+	} else{
+		printf("Reprovado\n");
+	}
+	
+	
+	
+	
+	return 0;
+}
