9d7edd7d048592ce0030f84dec63e1f8f59461c8
Joao Pedro <jaauhp@gmail.com>
2023-10-27 11:02:48 -0300

    update

---
+++ ex012 break e continue/break.c
@@ -0,0 +1,17 @@
+int main(int argc, char const *argv[])
+{
+    int i;
+
+    for (i=1; i<=10; i++)
+    {
+        printf("%d",i);
+
+        if (i==5)
+        {
+            break;
+        }
+        
+    }
+    
+    return 0;
+}
